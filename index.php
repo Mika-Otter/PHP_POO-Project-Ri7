a6fc1e5a91c6262bc389bd37717160865aa27c6d
Mika Otter <mi.otter.side@gmail.com>
2024-03-12 09:58:03 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social Network - Ri7</title>
+</head>
+<body>
+    <nav>
+        <li>
+            <ul></ul>
+            <ul></ul>
+            <ul></ul>
+        </li>
+    </nav>
+    <section class="title">
+
+    </section>
+    
+</body>
+</html>
