96f2913505f2687b985ab38f8444521352130f1a
Mika Otter <mi.otter.side@gmail.com>
2024-03-13 21:58:17 +0100

    DONE: set-up thread. Begin add POST

--- views/thread/index.php
+++ views/thread/index.php
@@ -1,5 +1,22 @@
-<h1>Fil d'actualité</h1>
+<head>
+    <link rel="stylesheet" href="./public/styles/thread.css">
+</head>
+<main>
+<h1 id="homeTitle">F<span>rrr</span>d</h1>
+<section class="post">
 <?php foreach($thread as $post): ?>
-    <h2> <a href="/POO_Project/thread/lire/<?= $post["ID"] ?>"><?= $post["title"]?> </a></h2> 
-    <p><?= $post["content"]?></p>
+    <div class="post__ctn">
+        <h2> <a id="link" href="/POO_Project/thread/lire/<?= $post["ID"] ?>"><?= $post["title"]?> </a></h2> 
+        <p><?= $post["content"]?></p>
+        <span><?= $post["author"] ?></span>
+        <span><?= $post["date_created"] ?></span>
+    </div>
 <?php endforeach; ?>
+</section>
+<section class="post__add">
+    <form action="" method="post">
+        <label for="addPost">Dîtes nous tout <?= $_SESSION["userName"] ?>:</label>
+        <input type="textarea" name="addPost"/>
+    </form>
+</section>
+</main>
